285e4041458984f76ff928b18ef8b0dba41fb608
Visethboti <visethboti@gmail.com>
2023-03-29 09:14:48 +0700

    Working with select component

--- src/main/java/com/example/customcamundatasklist/util/JsonToHtml.java
+++ src/main/java/com/example/customcamundatasklist/util/JsonToHtml.java
@@ -12,42 +12,22 @@ public class JsonToHtml {
 	}
 
 	public String getHtml(String strJsonData) throws JSONException {
-
-		// System.out.println(new JSONObject(strJsonData).get("components").getClass());
 		return jsonToHtml(new JSONObject(strJsonData).get("components"));
-		// return "Hello world";
 	}
 
 	private String jsonToHtml(Object obj) throws JSONException {
 		StringBuilder html = new StringBuilder();
-
 		String input;
 
 		JSONArray array = (JSONArray) obj;
 		for (int i = 0; i < array.length(); i++) {
 			JSONObject component = (JSONObject) array.get(i);
-			// System.out.println(component.get("type"));
-			// System.out.println(array.get(i));
-//			label = componentToLabel(component);
 			input = componentToInput(component);
-//			html.append(label);
 			html.append(input);
 		}
 
 		return html.toString();
 	}
-
-//	private String componentToLabel(JSONObject component) throws JSONException {
-//		String label = "";
-//
-//		if (component.get("type").equals("text")) {
-//			label = "<label>" + component.get("text") + "</label><br>";
-//		} else {
-//			label = "<label>" + component.get("label") + "</label><br>";
-//		}
-//
-//		return label;
-//	}
 
 	private String componentToInput(JSONObject component) throws JSONException {
 		String input = "";
@@ -92,10 +72,11 @@ public class JsonToHtml {
 				input += "<label>" + c.get("label") + "</label>";
 			}
 			break;
-		case "taglist":
+		case "select":
 			input += "<label>" + component.get("label") + "</label><br>";
 			JSONArray array2 = (JSONArray) component.get("values");
-			input += "<select name=\"" + component.get("key") + "\">";
+			input += "<select name=\"" + component.get("key") + "\"required>";
+			input += "<option value=\"\" selected disabled hidden>Choose here</option>";
 			for (int i = 0; i < array2.length(); i++) {
 				JSONObject c = (JSONObject) array2.get(i);
 				input += "<option value=\"" + c.get("value") + "\">" + c.get("label") + "</option>";
